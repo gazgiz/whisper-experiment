b5f0fb133fbcfb17d1a046ef9c1abb5afa2e8020
Aaron Boxer <boxerab@protonmail.com>
2024-06-06 14:44:03 -0400

    switch to gpu

--- stt/server.py
+++ stt/server.py
@@ -30,7 +30,7 @@ app.add_middleware(
 model = whisper.load_model("base")
 
 # Load the Coqui TTS model for multilingual support
-tts = TTS(model_name="tts_models/multilingual/multi-dataset/xtts_v2", progress_bar=False, gpu=False)
+tts = TTS(model_name="tts_models/multilingual/multi-dataset/xtts_v2", progress_bar=False, gpu=True)
 
 # Set default speaker ID and language
 default_speaker_id = 'Andrew Chipper'  # Replace with any speaker ID from the list
@@ -118,5 +118,5 @@ async def process_audio_chunk(data, websocket: WebSocket):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, ws_max_size=10485760, ws_ping_interval=30, ws_ping_timeout=60)  # Increase the max size to 10MB, set ping interval and timeout
+    uvicorn.run(app, host="0.0.0.0", port=8000, ws_max_size=10485760, ws_ping_interval=20, ws_ping_timeout=10, ws_close_timeout=10)  # Increase the max size to 10MB, set ping interval and timeout
 
